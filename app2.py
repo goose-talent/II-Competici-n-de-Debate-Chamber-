8c935bcc18f095ec623fbcfc77f6a4be8632ffac
David Fariña <davidfarinalema@gmail.com>
2026-05-18 12:49:36 +0200

    arreglo

--- app2.py
+++ app2.py
@@ -53,7 +53,7 @@ st.markdown(
 )
 with st.form("formulario_inscripcion"):
     
-    torneo = st.subheader("II Competición de Debate Escolar JMD CHAMBERI *")
+    torneo = st.subheader("I Competición de Debate Escolar JMD CHAMBERI *")
     torneo = "II Competición de Debate Escolar JMD CHAMBERI"
 
     st.subheader("Datos del centro")
